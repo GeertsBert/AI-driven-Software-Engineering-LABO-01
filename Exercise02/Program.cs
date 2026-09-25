476253de494fee649c54f9034bc5373fc659b623
GeertsBert <bert.geerts@student.howest.be>
2026-09-25 14:29:42 +0200

    Add Exercise02

---
+++ Exercise02/Program.cs
@@ -0,0 +1,12 @@
+﻿//Write ipv WriteLine zo komt de input netjes naast de text.
+Console.Write("value A: ");
+string valueA = Console.ReadLine();
+
+Console.Write("value B: ");
+string valueB = Console.ReadLine();
+
+int a = int.TryParse(valueA);
+int b = int.TryParse(valueB);
+int sum = a + b;
+
+Console.WriteLine($"The sum of {a} and {b} is {sum}");
